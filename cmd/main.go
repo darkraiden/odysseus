82e0509bdbca02fec37596b9102f9af7eb62adb7
Davide Di Mauro <dadimaur@microsoft.com>
2020-02-21 15:47:13 +0000

    read config using viper

--- cmd/main.go
+++ cmd/main.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/darkraiden/odysseus/internal/cloudflare"
 	"github.com/darkraiden/odysseus/internal/logs"
 	"github.com/darkraiden/odysseus/internal/template"
 	"github.com/darkraiden/odysseus/internal/whatsmyip"
+	"github.com/spf13/viper"
 )
 
-var requiredVars = []string{"CF_API_KEY", "CF_API_EMAIL", "CF_ZONE_NAME"}
-
 func init() {
-	for _, rv := range requiredVars {
-		if os.Getenv(rv) == "" {
-			panic(fmt.Errorf("environment variable '%s' not set", rv))
-		}
+	// Read configuration file
+	viper.SetConfigName("cloudflare")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
 	}
 }
 
 func main() {
-	api, err := cloudflare.New(cloudflare.Config{APIKey: os.Getenv("CF_API_KEY"), Email: os.Getenv("CF_API_EMAIL"), ZoneName: os.Getenv("CF_ZONE_NAME")})
-	if err != nil {
-		panic(err)
-	}
-
-	records, err := api.GetDNSRecords([]string{fmt.Sprintf("www.%s", os.Getenv("CF_ZONE_NAME"))})
-	if err != nil {
-		panic(err)
-	}
-
 	// wire our template and logger together
 	logger := logs.NewLogger()
-
 	l := template.New(logger, "Info")
 
 	l.Log("Welcome to Odysseus")
+
+	api, err := cloudflare.New(cloudflare.Config{APIKey: viper.Get("cloudflare.api_key").(string), Email: viper.Get("cloudflare.email").(string), ZoneName: viper.Get("cloudflare.zone_name").(string)})
+	if err != nil {
+		panic(err)
+	}
+
+	records, err := api.GetDNSRecords(viper.Get("cloudflare.records").([]interface{}))
+	if err != nil {
+		panic(err)
+	}
 
 	ip, err := whatsmyip.GetLocalIP()
 	if err != nil {
