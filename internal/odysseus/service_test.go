756da73c664bda96e85d70f73cad92f2ddf77d55
Davide Di Mauro <davidedimauro88@gmail.com>
2023-02-21 14:28:59 +0000

    Add tests for odysseus.NewService

--- internal/odysseus/service_test.go
+++ internal/odysseus/service_test.go
@@ -2,17 +2,48 @@ package odysseus_test
 
 import (
 	"errors"
+	"testing"
+
 	"github.com/darkraiden/odysseus/internal/mock/mockdns"
 	mockip "github.com/darkraiden/odysseus/internal/mock/mockip"
 	"github.com/darkraiden/odysseus/internal/odysseus"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestNewService(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
+	t.Run("returns an error when DNS.Manager is nil", func(t *testing.T) {
+		mockIP := mockip.NewMockGetter(ctrl)
+		s, err := odysseus.NewService(nil, mockIP)
+		require.Nil(t, s)
+		require.Error(t, err)
+	})
+
+	t.Run("returns an error when ipaddress.Getter is nil", func(t *testing.T) {
+		mockDNS := mockdns.NewMockManager(ctrl)
+		s, err := odysseus.NewService(mockDNS, nil)
+		require.Nil(t, s)
+		require.Error(t, err)
+	})
+
+	t.Run("returns an error when both ipaddress.Getter and DNS.Manager are nil", func(t *testing.T) {
+		s, err := odysseus.NewService(nil, nil)
+		require.Nil(t, s)
+		require.Error(t, err)
+	})
+
+	t.Run("test content of *Service", func(t *testing.T) {
+		mockDNS := mockdns.NewMockManager(ctrl)
+		mockIP := mockip.NewMockGetter(ctrl)
+
+		s, err := odysseus.NewService(mockDNS, mockIP)
+		require.Nil(t, err)
+		require.NotNil(t, s)
+	})
 }
 
 func TestService_UpdateDNSWithLocalIP(t *testing.T) {
